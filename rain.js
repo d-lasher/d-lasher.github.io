60ffbada8e485e06910f71c69273b86988b82393
dana@cdrdev.com <dana@cdrdev.com>
2023-05-10 19:31:59 -0600

    rainy day II

--- rain.js
+++ rain.js
@@ -11,17 +11,15 @@ function updateRainBox(forecastUpdated,conditionsUpdated) {
         let wx = getCurrentWx()
 
         if (wx.hasOwnProperty("rainratein") == true) {
-            document.querySelector('#rainratein').innerHTML = parseFloat(wx.rainratein).toFixed(2);
+            document.querySelector('#rainratein_ex').innerHTML = parseFloat(wx.rainratein).toFixed(2);
 
-            document.querySelector('#rain_data').style.display = "block"
-            document.querySelector('#rain_hr_value').innerHTML = parseFloat(wx.hourlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_day_value').innerHTML = parseFloat(wx.dailyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_wk_value').innerHTML = parseFloat(wx.weeklyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_mn_value').innerHTML = parseFloat(wx.monthlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_yr_value').innerHTML = parseFloat(wx.yearlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_hr_value_ex').innerHTML = parseFloat(wx.hourlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_day_value_ex').innerHTML = parseFloat(wx.dailyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_wk_value_ex').innerHTML = parseFloat(wx.weeklyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_mn_value_ex').innerHTML = parseFloat(wx.monthlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_yr_value_ex').innerHTML = parseFloat(wx.yearlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
         } else {
-            document.querySelector('#rainratein').innerHTML = "0.0";
-            document.querySelector('#rain_data').style.display = "hidden"
+            document.querySelector('#rainratein_ex').innerHTML = "0.0";
         }
     }
 
