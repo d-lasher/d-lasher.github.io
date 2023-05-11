706a0dbbc0c7c7ebb5890eed91349e3203943beb
dana@cdrdev.com <dana@cdrdev.com>
2023-05-10 19:38:31 -0600

    again

--- rain.js
+++ rain.js
@@ -13,11 +13,11 @@ function updateRainBox(forecastUpdated,conditionsUpdated) {
         if (wx.hasOwnProperty("rainratein") == true) {
             document.querySelector('#rainratein_ex').innerHTML = parseFloat(wx.rainratein).toFixed(2);
 
-            document.querySelector('#rain_hr_value_ex').innerHTML = parseFloat(wx.hourlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_day_value_ex').innerHTML = parseFloat(wx.dailyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_wk_value_ex').innerHTML = parseFloat(wx.weeklyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_mn_value_ex').innerHTML = parseFloat(wx.monthlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
-            document.querySelector('#rain_yr_value_ex').innerHTML = parseFloat(wx.yearlyrainin).toFixed(1) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_hr_value_ex').innerHTML = parseFloat(wx.hourlyrainin).toFixed(2) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_day_value_ex').innerHTML = parseFloat(wx.dailyrainin).toFixed(2) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_wk_value_ex').innerHTML = parseFloat(wx.weeklyrainin).toFixed(2) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_mn_value_ex').innerHTML = parseFloat(wx.monthlyrainin).toFixed(2) + "<span class='rain_units'> in</span>";
+            document.querySelector('#rain_yr_value_ex').innerHTML = parseFloat(wx.yearlyrainin).toFixed(2) + "<span class='rain_units'> in</span>";
         } else {
             document.querySelector('#rainratein_ex').innerHTML = "0.0";
         }
